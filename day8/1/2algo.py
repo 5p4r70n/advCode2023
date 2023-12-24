3f898c0809e57293f9d6c47aa40ffc6acbd2f5bc
Jothish <jothishwp8@outlook.com>
2023-12-24 16:35:24 +0530

    day8_1

---
+++ day8/1/2algo.py
@@ -0,0 +1,56 @@
+fielData=open("data.txt","r").readlines()
+route=[int(i) for i in fielData[0].strip().replace("L","0").replace("R","1")]
+
+lanes={i.split("=")[0].strip():i.replace("(","").replace(")","").replace(" ","").split("=")[1].strip().split(",")  for i in fielData[1:] if i.strip()!='' }
+
+# count=0
+finalKey="ZZZ"
+
+
+loopCount=0
+def findZ(initKey,count):
+    global loopCount
+    print(initKey,count)
+    
+
+    # #to stop inifinite loop
+    # if loopCount>=10:
+    #     return "closed"
+
+    if  count >= len(route) : 
+        print("clear Count : ",count)
+        findZ(initKey,0)
+        
+    else:    
+        loopCount+=1
+        if lanes[initKey][route[count]]== finalKey:
+            print("finally",loopCount)
+            return
+        else:
+            print("firing",lanes[initKey][route[count]],count)
+            findZ(lanes[initKey][route[count]],count+1)
+
+        
+    
+    
+a=True   
+initKey="AAA"
+count=0
+while a:
+    
+    if  count >= len(route) : 
+        print("clear Count : ",count)
+        count=0
+        continue
+
+    else:    
+        loopCount+=1
+        if initKey== finalKey:
+            print("finally",loopCount-1)
+            a=False
+            break
+        else:
+            # print("firing",lanes[initKey][route[count]],count)
+            initKey=lanes[initKey][route[count]]
+            
+    count+=1
